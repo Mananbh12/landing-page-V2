fba67aa4b8384840323ed9e52d259a8706ffbe5f
Arya Putra <mananbhrdwj@gmail.com>
2025-04-12 14:18:23 +0200

    recent projects added

--- components/Grid.tsx
+++ components/Grid.tsx
@@ -1,12 +1,13 @@
 import { gridItems } from '@/data';
-import { BentoGrid, BentoGridItem } from './ui/BentoGridItems'; // Nouveau composant client
+import { BentoGrid, BentoGridItem } from './ui/BentoGrid';
 
 const Grid = () => {
   return (
     <section id="about" className="relative z-20">
       <BentoGrid>
-        {gridItems.map(({img, description, className}) => (
+        {gridItems.map(({ img, description, className }, index) => (
           <BentoGridItem
+            key={index} // Ajout de la prop key
             img={img}
             description={description}
             className={className}
